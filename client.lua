6c5c9dd0160d020a5be0faf5c1cc1c5d7e05691e
Pyth3rEx <info@pyth3rex.com>
2019-01-28 18:23:42 +0100

    Fixed PlaySoudFromCoords
    Sound now plays from the nearset station to the player. Now trying to find a good sounding alarm

--- client.lua
+++ client.lua
@@ -1,41 +1,64 @@
+------------------------------ Variables
 local fireHornLocation = {
+  { x = 216.85, y = -1648.05, z = 30.72, name = "Davis Station"},
   { x = 1194.27, y = -1464.01, z = 36.65, name = "El Burro Station"},
-  { x = 206.5, y = -1339.75, z = 31.59, name = "Davis Station"},
+  { x = -634.79, y = -124.02, z = 39.01, name = "Rockford Hills Station"},
 }
 
---[[
-RegisterCommand('spawnFire', function(source, args, rawCommand)
-  spawnFire()
-end, false)
-
-
-Citizen.CreateThread(function()
-  TriggerEvent('chat:addSuggestion', '/fireAlarm', 'send the fireDispatch main function', { {name='station number', help="the station's number"} })
-end)
---]]
-
+local fireSpawnLocation = {
+  { x = 1164.35, y = -1463.6, z = 34.84, name = "El Burro Station", id = 1, isFuel = false}, --fire // id = type of fire
+}
+------------------------------ Dispatch
 RegisterNetEvent("triggerSound")
 AddEventHandler("triggerSound", function()
-
-  local plyCoords = GetEntityCoords(GetPlayerPed(-1))
-
   ShowNotification("You have triggered the ~r~alarm~w~!")
+  local plX, plY, plZ = table.unpack(GetEntityCoords(GetPlayerPed(-1), true)) --Gets player XYZ
+  local nearestStation
 
   for i = 1, #fireHornLocation, 1 do
     ShowNotification(fireHornLocation[i].name.." responding!")
-    --TriggerClientEvent("chatMessage", "[Dispatch]", {255,0,0}, fireHornLocation[i].name.." responding!")
-    
-    --[[
-    local distance = Vdist(plyCoords.x, plyCoords.y, plyCoords.z, fireHornLcation[i].x, fireHornLocation[i].y, fireHornLocation[i].z);
 
+    local distDiff = Vdist(plX, plY, plZ, fireHornLocation[i].x, fireHornLocation[i].y, fireHornLocation[i].z) --Gets distance between player and firestation[i]
+    local nearestStationDiff
 
-    if(distance < 500.0) then
-      PlaySoundFromCoord(-1, "scanner_alarm_os", fireHornLocation[i].x, fireHornLocation[i].y, fireHornLocation[i].z, "dlc_xm_iaa_player_facility_sounds", true, 500, false)
+    if nearestStation == nil then --if there is no nearest station yet (first run) then...
+      nearestStation = i
+      nearestStationDiff = Vdist(plX, plY, plZ, fireHornLocation[i].x, fireHornLocation[i].y, fireHornLocation[i].z) --Gets distance between player and firestation[i]
+    else -- if there already a value attached to "nearestStation"
+      nearestStationDiff = Vdist(plX, plY, plZ, fireHornLocation[nearestStation].x, fireHornLocation[nearestStation].y, fireHornLocation[nearestStation].z) --Gets distance between player and nearest station so far
     end
-    --]]
-    
+
+    if distDiff <= nearestStationDiff then -- if new station is the closest yet
+      nearestStation = i -- assign new closest station
+      print(nearestStation)
+    end
   end
+  PlaySoundFromCoord(i, "scanner_alarm_os", fireHornLocation[nearestStation].x, fireHornLocation[nearestStation].y, fireHornLocation[nearestStation].z, "dlc_xm_iaa_player_facility_sounds", 1, 500, 0) --Plays sound from nearest station
 end)
+
+------------------------------ Fire
+RegisterCommand("startFire", function(source, args, rawCommand)
+  spawnFire()
+end, false)
+
+function spawnFire()
+  local i = math.random(#fireSpawnLocation)
+
+  local model = GetHashKey("buccaneer")
+	RequestModel(model)
+	while not HasModelLoaded(model) do
+		Citizen.Wait(0)
+	end
+
+  if(fireSpawnLocation[i].id == 0) then
+    CreateVehicle(model,fireSpawnLocation[i].x, fireSpawnLocation[i].y, fireSpawnLocation[i].z, 0.0, true, false)--Spawns vehicle
+    StartScriptFire(fireSpawnLocation[i].x, fireSpawnLocation[i].y-1, fireSpawnLocation[i].z, 25, fireSpawnLocation[i].isFuel) --spawn fire
+  else
+    StartScriptFire(fireSpawnLocation[i].x, fireSpawnLocation[i].y, fireSpawnLocation[i].z, 25, fireSpawnLocation[i].isFuel) --spawn fire
+  end
+end
+
+------------------------------ Seperate functions
 
 function ShowNotification(text)
   SetNotificationTextEntry("STRING")
