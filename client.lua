e98085590f6d134ef26b67ab41e5e36211141ed3
Pyth3rEx <info@pyth3rex.com>
2019-02-05 01:38:33 +0100

    Flowcharts and fire stuff
    Added flowcharts so we know what does what.
    Added the awsome looking particles. Still need to figure out how to get rid of it lol.

--- client.lua
+++ client.lua
@@ -1,4 +1,7 @@
------------------------------- Variables
+------------------------------           ------------------------------
+------------------------------ Variables ------------------------------
+------------------------------           ------------------------------
+
 local fireHornLocation = {
   { x = 216.85, y = -1648.05, z = 30.72, name = "Davis Station"},
   { x = 1194.27, y = -1464.01, z = 36.65, name = "El Burro Station"},
@@ -6,17 +9,21 @@ local fireHornLocation = {
 }
 
 local fireSpawnLocation = {
-  { x = 1164.35, y = -1463.6, z = 34.84, name = "El Burro Station", id = 1, isFuel = false}, --fire // id = type of fire
+  { x = 1161.0, y = -1452.53, z = 34.72, name = "El Burro Station", id = 1, isFuel = false}, --fire // id = type of fire
 }
------------------------------- Dispatch
+
+------------------------------          ------------------------------
+------------------------------ Dispatch ------------------------------
+------------------------------          ------------------------------
+
 RegisterNetEvent("triggerSound")
 AddEventHandler("triggerSound", function()
-  ShowNotification("You have triggered the ~r~alarm~w~!")
+  --ShowNotification("You have triggered the ~r~alarm~w~!")
   local plX, plY, plZ = table.unpack(GetEntityCoords(GetPlayerPed(-1), true)) --Gets player XYZ
   local nearestStation
 
   for i = 1, #fireHornLocation, 1 do
-    ShowNotification(fireHornLocation[i].name.." responding!")
+    --ShowNotification(fireHornLocation[i].name.." responding!")
 
     local distDiff = Vdist(plX, plY, plZ, fireHornLocation[i].x, fireHornLocation[i].y, fireHornLocation[i].z) --Gets distance between player and firestation[i]
     local nearestStationDiff
@@ -35,7 +42,7 @@ AddEventHandler("triggerSound", function()
   end
 
 
-  ---- PLAYING THE SOUND IN A RIDDM
+  ---- PLAYING THE SOUND IN A RYTHM
   for i = 1, 10, 1 do -- repeat to make it sound like an alarm
     for i = 1, 10, 1 do -- used to make it louder
       PlaySoundFromCoord(i, "scanner_alarm_os", fireHornLocation[nearestStation].x, fireHornLocation[nearestStation].y, fireHornLocation[nearestStation].z, "dlc_xm_iaa_player_facility_sounds", 1, 500, 0) --Plays sound from nearest station
@@ -51,29 +58,46 @@ AddEventHandler("triggerSound", function()
   end
 end)
 
------------------------------- Fire
+------------------------------      ------------------------------
+------------------------------ Fire ------------------------------
+------------------------------      ------------------------------
 RegisterCommand("startFire", function(source, args, rawCommand)
-  spawnFire()
+  TriggerServerEvent("potato:syncFire", source); -- syncs fire
 end, false)
 
-function spawnFire()
-  local i = math.random(#fireSpawnLocation)
+RegisterNetEvent("syncCallback")
+AddEventHandler("syncCallback", function()
+  local i = math.random(#fireSpawnLocation) -- Choses a random spawn location
 
-  local model = GetHashKey("buccaneer")
-	RequestModel(model)
+  -- Used for calls wich a vehicle:
+  local model = GetHashKey("buccaneer") -- Get car's hash
+  RequestModel(model) -- Car spawing stuff
+  
+  -- Stuff to avoid crash
 	while not HasModelLoaded(model) do
 		Citizen.Wait(0)
-	end
+  end
+  
+  if not HasNamedPtfxAssetLoaded("core") then
+    RequestNamedPtfxAsset("core")
+      while not HasNamedPtfxAssetLoaded("core") do
+        Wait(1)
+      end
+  end
+  SetPtfxAssetNextCall("core")
 
   if(fireSpawnLocation[i].id == 0) then
     CreateVehicle(model,fireSpawnLocation[i].x, fireSpawnLocation[i].y, fireSpawnLocation[i].z, 0.0, true, false)--Spawns vehicle
-    StartScriptFire(fireSpawnLocation[i].x, fireSpawnLocation[i].y-1, fireSpawnLocation[i].z, 25, fireSpawnLocation[i].isFuel) --spawn fire
+    StartScriptFire(fireSpawnLocation[i].x, fireSpawnLocation[i].y+1.5, fireSpawnLocation[i].z-1, 25, fireSpawnLocation[i].isFuel) --spawn fire
   else
     StartScriptFire(fireSpawnLocation[i].x, fireSpawnLocation[i].y, fireSpawnLocation[i].z, 25, fireSpawnLocation[i].isFuel) --spawn fire
+    StartParticleFxLoopedAtCoord("ent_ray_heli_aprtmnt_h_fire", fireSpawnLocation[i].x, fireSpawnLocation[i].y, fireSpawnLocation[i].z-0.7, 0.0, 0.0, 0.0, 1.0, false, false, false, false)  
   end
-end
+end)
 
------------------------------- Seperate functions
+------------------------------                    ------------------------------
+------------------------------ Seperate functions ------------------------------
+------------------------------                    ------------------------------
 
 function ShowNotification(text)
   SetNotificationTextEntry("STRING")
--- server.lua
+++ server.lua
@@ -1,12 +1,4 @@
-RegisterCommand("fireAlarm", function(source, args, rawCommand)
-  TriggerClientEvent("triggerSound", source)
-end, false)
-
-RegisterCommand("startFire", function(source, args, rawCommand)
-  spawnFire()
-end, false)
-
--- FIRE FUNCTIONS
-local fireSpawnLocation = {
-  { x = 1194.33, y = -1449.03, z = 35.16, name = "El Burro Station"}, --fire
-}
+RegisterServerEvent("potato:syncFire")
+AddEventHandler("potato:syncFire", function()
+  TriggerClientEvent("syncCallback", -1) -- Asks every client to spawn fire
+end)
