1cd8034a9e0f852932cc48609b44d69c48f7a026
Pyth3rEx <info@pyth3rex.com>
2019-02-13 03:20:30 +0100

    Added Firestation locations

--- client.lua
+++ client.lua
@@ -6,6 +6,10 @@ local fireHornLocation = {
   { x = 216.85, y = -1648.05, z = 30.72, name = "Davis Station"},
   { x = 1194.27, y = -1464.01, z = 36.65, name = "El Burro Station"},
   { x = -634.79, y = -124.02, z = 39.01, name = "Rockford Hills Station"},
+  { x = -379.34, y = 6118.42, z = 31.85, name = "Paleto Fire Station"},
+  { x = 1691.79, y = 3584.92, z = 36.6, name = "Sandy Shores Fire Station"},
+  { x = -1030.88, y = -2374.77, z = 20.61, name = "Los Santos Airport Fire Station"},
+  { x = -1189.27, y = -1784.38, z = 15.62, name = "Vespucci Beach LifeGuard Station"},
 }
 
 local fireSpawnLocation = {
